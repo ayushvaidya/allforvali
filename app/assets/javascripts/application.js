fa8fd3480a66010996b8fc482376a31c902c4300
Rick Shukla <shuklapossible@gmail.com>
2018-12-13 16:13:52 -0500

    wip: tested locally w/o jquery - works

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,13 +22,17 @@ $(document).on("turbolinks:load", function() {
     const players = Array.from(document.querySelectorAll('.js-player')).map(p => new Plyr(p));
 });
 
-(function() {
-    console.log('iife')
-    $("audio").on("play", function() {
-        console.log('on play')
-        $("audio").not(this).each(function(index, audio) {
-            console.log('pause')
-            audio.pause();
-        });
-    });
-})()
+window.onload = function() {
+    document.addEventListener("play", function(event) {
+        audio_elements = document.body.getElementsByTagName("audio")
+        console.log('audio_elements', audio_elements)
+        for(i=0; i < audio_elements.length; i++) {
+            audio_element = audio_elements[i];
+            console.log('audio_element', audio_element, event.target)
+            if (audio_element !== event.target.firstElementChild) {
+                console.log('pause')
+                audio_element.pause();
+            }
+        }
+    }, false);
+  };
